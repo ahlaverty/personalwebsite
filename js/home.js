35b2530ab8f91386bb50b4e60f0593ea41e7b311
Aubrey H. Laverty <ahlaverty@aubreyhlaverty.io>
2019-01-31 15:56:04 -0700

    updated js

--- js/home.js
+++ js/home.js
@@ -1,11 +1,24 @@
 $().ready(function(){
 
+	//check for nav
 	hideNav();
 
 	$(window).resize(function(){
 		hideNav();
 
 	});
+
+
+	$(".navbar-toggler").click(function(){
+		if($(this).attr("aria-expanded") === "false"){
+			$(".navbar-toggler-icon").removeClass("fa-ellipsis-v").addClass("fa-times");
+		}
+		else{
+			$(".navbar-toggler-icon").addClass("fa-ellipsis-v").removeClass("fa-times");
+		}
+	});
+
+
 		
 	function hideNav(){
 		$(".nav-link").click(function(){
